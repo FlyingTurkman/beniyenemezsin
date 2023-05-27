06d5ed8797d4f2641e07dc736d4a9916f25c9145
FlyingTurkman <dytdenizarrli@hotmail.com>
2023-05-27 20:29:17 +0300

    master

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,15 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
     <Head>
       <title>Beni Yenemezsin</title>
+      <meta property="twitter:image" content="/public/logo.png"></meta>
+      <meta property="twitter:card" content="/public/logo.png"></meta>
+      <meta property="twitter:title" content="Beni Yenemezsin"></meta>
+      <meta property="twitter:description" content="Sıra tabanlı matematik oyunu. 0 veya -1 olan kaybeder."></meta>
+      <meta property="description" content="Sıra tabanlı matematik oyunu. 0 veya -1 olan kaybeder." />
+      <meta property="og:image" content="/public/logo.png"></meta>
+      <meta property="og:title" content="Beni Yenemezsin"></meta>
+      <meta property="og:description" content="Sıra tabanlı matematik oyunu. 0 veya -1 olan kaybeder."/>
+      <meta property="og:url" content="https://beniyenemezsin.vercel.app"></meta>
     </Head>
     <MainMenu/>
     <Component {...pageProps} />
