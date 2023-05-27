cf2ef460569054d5270987fffa264c711e95387d
FlyingTurkman <dytdenizarrli@hotmail.com>
2023-05-27 20:24:12 +0300

    master

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import MainMenu from '../components/MainMenu'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <Head>
+      <title>Beni Yenemezsin</title>
+    </Head>
     <MainMenu/>
     <Component {...pageProps} />
     </>
